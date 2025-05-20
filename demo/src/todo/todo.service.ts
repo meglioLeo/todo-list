deaa913b72e0738559755e3ae5ce252d1239e758
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-05-20 22:20:26 +0200

    Added comments in read todos method

--- demo/src/todo/todo.service.ts
+++ demo/src/todo/todo.service.ts
@@ -8,12 +8,12 @@ const DB_FILE = path.join(__dirname, 'todo.json');  // path to the database file
 @Injectable()
 export class TodoService {
     
-    private readTodos(): Todo[]{
-        if(!fs.existsSync(DB_FILE)){
+    private readTodos(): Todo[]{          // read the todos from the database file
+        if(!fs.existsSync(DB_FILE)){      // check if the file exists
             return []
         }
-        const data = fs.readFileSync(DB_FILE, 'utf-8'); 
-        return JSON.parse(data) as Todo[];                       
+        const data = fs.readFileSync(DB_FILE, 'utf-8');   // read the file synchronously
+        return JSON.parse(data) as Todo[];                // parse the file content to Todo array
     }
 
 }
