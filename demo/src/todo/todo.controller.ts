09ca7cd71ba02b328f006b3033e32af45c4736b1
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-05-20 23:14:05 +0200

    Created the controller to create a new todo

--- demo/src/todo/todo.controller.ts
+++ demo/src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { Todo } from './entities/todo.entity';
 
@@ -15,5 +15,10 @@ export class TodoController {
     @Get(':id')
     getTodoById(@Param('id') id:string): Todo | null {
         return this.todoService.readTodoById(id); // return todo by id
+    }
+
+    @Post()
+    createTodo(@Body() todoData: any): Todo{
+        return this.todoService.createTodo(todoData); // create a new todo
     }
 }
