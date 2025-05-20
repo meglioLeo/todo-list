2bf83ac17608d520800d54343b623cbc0c78305c
meglioLeo <leonardo.cresti@students.sarrocchi.it>
2025-05-20 20:28:21 +0200

    Added imports to the services file

--- demo/src/todo/todo.service.ts
+++ demo/src/todo/todo.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";        // makes the class injectable in Node.js
+import { Todo } from "./entities/todo.entity";    // import the Todo entity
+import * as fs from "fs";                           // allows to work with file 
+import * as path from "path";                       // allows to work with path
+
+const DB_FILE = path.join(__dirname, 'todo.json');  // path to the database file
 
 @Injectable()
 export class TodoService {
